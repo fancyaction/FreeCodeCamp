3973d8b038435d8c10b5aa074bf398090913b403
fancyaction <labcoatdisaster@gmail.com>
2018-02-21 15:01:51 -0600

    Initial commit

---
+++ Weather-App/js/main.js
@@ -0,0 +1,9 @@
+// User Story: I can see the weather in my current location.
+
+// User Story: I can push a button to toggle between Fahrenheit and Celsius.
+
+// User Story: I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
+
+// animated background
+
+// sound tied to weather
