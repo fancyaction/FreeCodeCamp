5100cf637b86960c7abe5d01f9a1bd8940c5f345
fancyaction <labcoatdisaster@gmail.com>
2018-02-22 14:50:33 -0600

    Added convert temperature feature

--- Weather-App/js/main.js
+++ Weather-App/js/main.js
@@ -1,21 +1,26 @@
 const cityName = document.querySelector('h3');
 const tempMaxMin = document.querySelector('.temp-max-min');
-const temp = document.querySelector('h1');
+const tempFormat = document.querySelector('.temp-format');
+const temp = document.querySelector('.temp');
 const icon = document.querySelector('.weather-icon');
+
+let celsius = '';
+let fahrenheit = '';
+let tempMax = 0;
+let tempMin = 0;
 
 
 // User Story: I can see the weather in my current location.
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getPosition);
+        navigator.geolocation.getCurrentPosition(getCoords);
     } else {
         console.log('Geolocation is not supported by this browser.');
     }
 }
 
-function getPosition(position) {
-    userLat = position.coords.latitude;
-    userLon = position.coords.longitude;
+function getCoords(position) {
+    const {latitude: userLat, longitude: userLon} = position.coords;
     endpoint = `https://fcc-weather-api.glitch.me/api/current?lat=${userLat}&lon=${userLon}`;
     fetch(endpoint)
         .then(blob => blob.json())
@@ -23,14 +28,33 @@ function getPosition(position) {
     }
 
 function setInfo(data) {
-    console.log(data);
-
     cityName.textContent = data.name;
-    tempMaxMin.innerHTML = `${data.main.temp_max}° MAX   ${data.main.temp_min}° MIN`;
-    temp.textContent = data.main.temp + ' °C';
+    tempMax = data.main.temp_max;
+    tempMin = data.main.temp_min;
+    celsius = Math.floor(data.main.temp);
+    fahrenheit = Math.floor((data.main.temp * 9/5) + 32);
+
+    tempMaxMin.innerHTML = `${tempMax}° MAX   ${tempMin}° MIN`;
+    temp.textContent = celsius;
     icon.innerHTML = data.weather[1].main;
 }
 // User Story: I can push a button to toggle between Fahrenheit and Celsius.
+function convertTemp() {
+    if (parseInt(temp.textContent) === celsius) {
+        temp.textContent = fahrenheit;
+        tempFormat.textContent = ' °F';
+        tempMaxMin.innerHTML = `${Math.floor((tempMax * 9/5) + 32)}° MAX   ${Math.floor((tempMin * 9/5) + 32)}° MIN`;
+    } else {
+        temp.textContent = celsius;
+        tempFormat.textContent = ' °C';
+        tempMaxMin.innerHTML = `${tempMax}° MAX   ${tempMin}° MIN`;
+    }
+}
+
+getLocation();
+
+
+tempFormat.addEventListener('click', convertTemp);
 
 // User Story: I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
 
