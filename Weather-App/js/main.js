5bd97596f9e2519549fac79f84cdd83b0b737659
fancyaction <labcoatdisaster@gmail.com>
2018-02-22 18:44:44 -0600

    Added weather icons feature

--- Weather-App/js/main.js
+++ Weather-App/js/main.js
@@ -1,13 +1,12 @@
 const cityName = document.querySelector('h3');
-const tempMaxMin = document.querySelector('.temp-max-min');
+const tempMax = document.querySelector('.temp-max');
+const tempMin = document.querySelector('.temp-min');
 const tempFormat = document.querySelector('.temp-format');
 const temp = document.querySelector('.temp');
 const icon = document.querySelector('.weather-icon');
+const weatherDetails = document.querySelector('.weather');
 
-let celsius = '';
-let fahrenheit = '';
-let tempMax = 0;
-let tempMin = 0;
+let celsius, fahrenheit, tempMaxNum, tempMinNum, weather;
 
 
 // User Story: I can see the weather in my current location.
@@ -20,34 +19,63 @@ function getLocation() {
 }
 
 function getCoords(position) {
-    const {latitude: userLat, longitude: userLon} = position.coords;
+    const { latitude: userLat, longitude: userLon } = position.coords;
     endpoint = `https://fcc-weather-api.glitch.me/api/current?lat=${userLat}&lon=${userLon}`;
     fetch(endpoint)
         .then(blob => blob.json())
         .then(data => setInfo(data));
-    }
+}
 
 function setInfo(data) {
-    cityName.textContent = data.name;
-    tempMax = data.main.temp_max;
-    tempMin = data.main.temp_min;
-    celsius = Math.floor(data.main.temp);
-    fahrenheit = Math.floor((data.main.temp * 9/5) + 32);
+    console.log(data);
 
-    tempMaxMin.innerHTML = `${tempMax}° MAX   ${tempMin}° MIN`;
+    cityName.textContent = data.name;
+    tempMaxNum = data.main.temp_max;
+    tempMinNum = data.main.temp_min;
+    celsius = Math.floor(data.main.temp);
+    fahrenheit = Math.floor((data.main.temp * 9 / 5) + 32);
+
+    tempMax.textContent = tempMaxNum + '°';
+    tempMin.textContent = tempMinNum  + '°';
     temp.textContent = celsius;
-    icon.innerHTML = data.weather[1].main;
+    weather = data.weather[0].main;
+    weatherDetails.textContent = weather;
 }
+
+// User Story: I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
+switch (weather) {
+    case 'Rain':
+        icon.appendChild(document.querySelector('.rainy'));
+        break;
+
+    case 'Thunderstorm':
+        icon.appendChild(document.querySelector('.thunder-storm'));
+        break;
+
+    case 'Snow':
+        icon.appendChild(document.querySelector('.flurries'));
+        break;
+
+    case 'Clear':
+        icon.appendChild(document.querySelector('.sunny'));
+    break;
+    
+    default:
+        icon.appendChild(document.querySelector('.cloudy'));
+}
+
 // User Story: I can push a button to toggle between Fahrenheit and Celsius.
 function convertTemp() {
     if (parseInt(temp.textContent) === celsius) {
         temp.textContent = fahrenheit;
         tempFormat.textContent = ' °F';
-        tempMaxMin.innerHTML = `${Math.floor((tempMax * 9/5) + 32)}° MAX   ${Math.floor((tempMin * 9/5) + 32)}° MIN`;
+        tempMax.innerHTML = `${Math.floor((tempMaxNum * 9 / 5) + 32)}°`;
+        tempMin.innerHTML = `${Math.floor((tempMinNum * 9 / 5) + 32)}°`;
     } else {
         temp.textContent = celsius;
         tempFormat.textContent = ' °C';
-        tempMaxMin.innerHTML = `${tempMax}° MAX   ${tempMin}° MIN`;
+        tempMax.textContent = `${Math.floor(tempMaxNum)}°`;
+        tempMin.textContent = `${Math.floor(tempMinNum)}°`;
     }
 }
 
@@ -56,7 +84,6 @@ getLocation();
 
 tempFormat.addEventListener('click', convertTemp);
 
-// User Story: I can see a different icon or background image (e.g. snowy mountain, hot desert) depending on the weather.
 
 // animated background
 
